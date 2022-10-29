e6d921afe2554c17fad4d0c614fde711bcde594f
cusmoritz <cus.moritz@gmail.com>
2022-10-29 07:13:30 -0600

    added search display function

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -39,6 +39,25 @@ const SearchBar = ({posts}) => {
                 })}
             />
             <PostSearch search={search} posts={posts}/>
+            {posts.map((eachPost) => {
+            if(eachPost.title.includes(search)) {
+                return (
+                    <p><b>Matches title in:</b> {eachPost.title}</p>
+                )
+
+            } if (eachPost.location.includes(search)) {
+                console.log('location matches', eachPost.location)
+                return (
+                    <p><b>Matches location in:</b> {eachPost.location}</p>
+                )
+            } if (eachPost.location.includes(search)) {
+                console.log('location matches', eachPost.description)
+                return (
+                    <p><b>Matches description in:</b> {eachPost.description}</p>
+                )
+            }
+            
+        })}
         </div>
     )
 }
